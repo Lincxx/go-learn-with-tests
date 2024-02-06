1c0c0dccb65053a2bd24c00049c4ea8c608a5f64
Jeremy Lincoln <bossco28@gmail.com>
2024-02-06 12:55:37 -0500

    Create a new fun to reduce on repeating code

--- hello_test.go
+++ hello_test.go
@@ -7,17 +7,21 @@ func TestHello(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying 'Hello World' when a empty satring is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
